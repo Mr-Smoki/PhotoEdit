1d5be1efcb9fe897a8834a9056fbcce81d1c3af2
Mr-Smoki <nekita10000@mail.ru>
2020-12-06 18:11:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/photoedit/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.photoedit;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static Bitmap selectedImage;
+    private static final int PICK_IMAGE=01;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE)
+        {
+            try {
+                //Получаем URI изображения, преобразуя его в Bitmap:
+                Uri imageUri = data.getData();
+                InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                selectedImage = BitmapFactory.decodeStream(imageStream);
+
+                Intent intent = new Intent(MainActivity.this,EditImageActivity.class);
+                startActivity(intent);
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void PickPhoto(View view) {
+        //Вызываем стандартную галерею для выбора изображения с помощью Intent.ACTION_PICK:
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        //Тип получаемых объектов - image:
+        photoPickerIntent.setType("image/*");
+        //Запускаем переход с ожиданием обратного результата в виде информации об изображении:
+        startActivityForResult(photoPickerIntent, PICK_IMAGE);
+
+    }
+}
